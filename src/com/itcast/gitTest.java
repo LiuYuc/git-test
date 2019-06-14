7ca61f2acb1669c8c507fcd34c4da86746475b9b
liuyucheng <1326466283@qq.com>
2019-06-14 16:41:06 +0800

    把本地工程添加到git

---
+++ src/com/itcast/gitTest.java
@@ -0,0 +1,9 @@
+package com.itcast;
+
+public class gitTest {
+    public static void main(String[] args){
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
